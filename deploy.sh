d8c6189a0f7eb7b5dbf2ff8c58d5d5e2582b77eb
Jonathan Wiemers <hi@jonathanwiemers.com>
2019-03-31 17:09:12 +0200

    updats deploy script to pull public folders branch before building hugo

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,9 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+cd public
+git pull origin master
+
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
